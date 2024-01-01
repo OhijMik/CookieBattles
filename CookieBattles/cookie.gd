171a498491a66c21ab48f98146fa4bdc0d7a3e2b
OhijMik <jihok148@gmail.com>
2023-12-31 20:33:24 -0500

    Added the milk enemy, player movement, and enemy movement
    - Added the milk enemy
    - Added player movement and enemy movement
    - Added the collision
    - Added the range of each characters

---
+++ CookieBattles/cookie.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+var chase
+var enemy_milk
+var speed = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	chase = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _physics_process(_delta):
+	if chase:
+		var direction = (enemy_milk.position - position).normalized()
+		velocity.x = direction.x * speed
+		velocity.y = direction.y * speed
+		move_and_slide()
+
+
+func _on_enemy_detection_body_entered(body):
+	if body.name == "Milk":
+		chase = true
+		print(body)
+		enemy_milk = body
+
+
+func _on_range_body_entered(body):
+	if body == enemy_milk:
+		chase = false
---
+++ CookieBattles/milk.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+var chase
+var cookie
+var speed = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	chase = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _physics_process(_delta):
+	if chase:
+		var direction = (cookie.position - position).normalized()
+		velocity.x = direction.x * speed
+		velocity.y = direction.y * speed
+		move_and_slide()
+
+
+func _on_cookie_detection_body_entered(body):
+	if body.name == "Cookie":
+		chase = true
+		print(body)
+		cookie = body
+
+
+func _on_range_body_entered(body):
+	if body == cookie:
+		chase = false
+
+
