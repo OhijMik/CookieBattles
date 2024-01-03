b38c2bd10399c626691a04b71970fa2d8f234a1f
OhijMik <jihok148@gmail.com>
2024-01-03 17:40:19 -0500

    Added a spawn system for the shop
    - Added a system where it automatically fills up the platform when you buy a cookie

--- CookieBattles/Shop_Icons/cookie_shop_icon.gd
+++ CookieBattles/Shop_Icons/cookie_shop_icon.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var cookie = preload("res://Cookies/cookie.tscn")
 var buyable = false
+var loop_break = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,11 +13,29 @@ func _ready():
 func _process(delta):
 	if buyable and Input.is_action_just_pressed("click"):
 		var cookie_temp = cookie.instantiate()
-		cookie_temp.position = Vector2(0,0)
-		get_node("../Cookies").add_child(cookie_temp)
-		print(cookie_temp.get_path())
-		buyable = false
-		self.queue_free()
+		
+		if global.cookie_list == []:
+			cookie_temp.position = Vector2(384,504)
+			get_node("../../Cookies").add_child(cookie_temp)
+			self.queue_free()
+		else:
+			for y in range(504, 760, 128):
+				for x in range(384, 1408, 128):
+					for cookie in global.cookie_list:
+						print(cookie.position)
+						print(Vector2(x,y))
+						if cookie.position == Vector2(x,y):
+							loop_break = false
+							break
+						else:
+							loop_break = true
+							continue
+					if loop_break: 
+						cookie_temp.position = Vector2(x,y)
+						get_node("../../Cookies").add_child(cookie_temp)
+						self.queue_free()
+						break
+				if loop_break: break
 
 
 func _on_area_2d_mouse_entered():
@@ -27,3 +46,4 @@ func _on_area_2d_mouse_entered():
 func _on_area_2d_mouse_exited():
 	scale = Vector2(1, 1)
 	buyable = false
+
